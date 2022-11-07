a63c1bb8103d6aea1571c1591c213a2a9ad96d65
Jesper Olsson Laine <jespols@student.chalmers.se>
2022-11-07 11:22:32 +0100

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [preact()],
-	site: `http://astro.build`,
+	integrations: [preact(),     sitemap({
+      filter: (page) => 
+			page !== 'https://blog.jeppzone.com/posts/my-carnivore-diaries' && 
+			page !== 'https://blog.jeppzone.com/hot-and-cold-therapy' && 
+			page !== 'https://blog.jeppzone.com/time-restricted-eating'
+    }),],
+	site: `https://blog.jeppzone.com`,
 });
